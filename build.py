34f75eb2fe26738ea662feec1607d6611920da9d
Emmanuel Peralta <manz@ringum.net>
2017-11-01 10:22:38 +0100

    Inserts the inline strings.

--- build.py
+++ build.py
@@ -10,34 +10,16 @@ from script import Table
 
 from utils.dump_battle_rooms import build_battle_text_patch
 from utils.dump_rooms import build_text_patch
-from utils.inline_strings import insert_dragon_feed_inline_strings, insert_battle_commands_strings
+from utils.inline_strings import insert_dragon_feed_inline_strings, insert_battle_commands_strings, insert_char_names, \
+    insert_inline_strings, insert_messages_strings, insert_battle_fixed
 
 
 def build_assembly(input, output, writer):
     asm = Program()
     asm.resolver.rom_type = RomType.high_rom
     asm.assemble_with_emitter(input, writer)
+    return asm
 
-
-# def build_room(input, output):
-#     asm = Program()
-#     asm.resolver.rom_type = RomType.high_rom
-#     buffer = BytesIO()
-#     sfc_emiter = SFCWriter(buffer)
-#     asm.assemble_with_emitter(input, sfc_emiter)
-#
-#     buffer.seek(0)
-#     uncompressed_data = buffer.read()
-#
-#     with open('/tmp/uncompressed.bin', 'wb') as uncompressed:
-#         uncompressed.write(uncompressed_data)
-#
-#     with open(output, 'wb') as fd:
-#         compressed = lz_compress(uncompressed_data) + b'\x00'
-#         compressed = struct.pack('<H', len(compressed) - 1) + compressed
-#         # fd.write()
-#         # fd.write(compressed)
-#         fd.write(compressed)
 
 if __name__ == '__main__':
     lang = 'mz'
@@ -48,8 +30,12 @@ if __name__ == '__main__':
         writer.begin()
 
         with open('bl.sfc', 'rb') as rom:
-            build_text_patch(rom, table, writer)
-            build_battle_text_patch(rom, table, writer)
+            address = build_text_patch(rom, table, writer, snes_to_rom(0xF00000))
+            print(f'Relocated dialog rooms end at {address + 0xC00000:#02x}')
+
+            address = build_battle_text_patch(rom, table, writer, address)
+
+            print(f'Relocated battle rooms end at {address + 0xC00000:#02x}')
 
         with open('src_assets/8x8_battle.dat', 'rb') as battle_font:
             writer.write_block(battle_font.read(), 0x261B40)
@@ -57,8 +43,17 @@ if __name__ == '__main__':
         with open('src_assets/8x8_font.dat', 'rb') as small_font:
             writer.write_block(small_font.read(), 0x8A000)
 
+        program = build_assembly('bl.s', './bl.ips', writer)
+        # get address for draw_inline_string_patched for code generation.
+        draw_inline_string_ref = program.resolver.current_scope['draw_inline_string_patched']
+
         insert_dragon_feed_inline_strings(writer, snes_to_rom(0xFC0000))
-        insert_battle_commands_strings(writer, snes_to_rom(0xFD0000))
-        build_assembly('bl.s', './bl.ips', writer)
+        end_of_battle_commands = insert_battle_commands_strings(writer, snes_to_rom(0xFD0000))
+        end_of_inline_strings = insert_inline_strings(writer, snes_to_rom(end_of_battle_commands + 1),
+                                                      draw_inline_string_ref)
+
+        insert_messages_strings(writer, snes_to_rom(end_of_inline_strings + 1))
+        insert_char_names(writer)
+        insert_battle_fixed(writer)
 
         writer.end()
