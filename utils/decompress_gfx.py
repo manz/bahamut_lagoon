3d0a6fb1c1c745993caac591f84d4330a4da6b76
Emmanuel Peralta <manz@ringum.net>
2018-04-12 19:31:05 +0200

    Adds missing lz_compress_gfx function.

--- utils/decompress_gfx.py
+++ utils/decompress_gfx.py
@@ -1,5 +1,49 @@
 import struct
 from a816.cpu.cpu_65c816 import snes_to_rom
+from io import BytesIO
+from utils.lz import lz_decompress, lz_compress, find_back_reference
+
+
+def compress_asset(data):
+    compressed = bytearray()
+    k = 0
+    while k < len(data):
+        back_reference = find_back_reference(data,
+                                             k,
+                                             search_patterns=True,
+                                             buffer_size=0x7ff,
+                                             max_length=0x3f,
+                                             min_length=3)
+        if back_reference:
+            reference, length = back_reference
+            # data_2    data_1
+            # aaaa aaaa | aa ll llll
+
+            if reference > 0x3ff:
+                compressed += b'\x1b'
+            else:
+                compressed += b'\x1a'
+
+            # reference = k - reference
+
+            a = reference & 0xFF
+            b = ((reference & 0x300) >> 2) | length
+
+
+            compressed.append(b)
+            compressed.append(a)
+
+            k += length
+        else:
+            # put byte in compressed stream
+            if data[k] in [0x1a, 0x1b]:
+                compressed += struct.pack('B', data[k]) + b'\x00\x00'
+            else:
+                compressed.append(data[k])
+            k += 1
+
+    compressed += b'\x1a\x00\x01'
+    return compressed
 
 
 def decompress_asset(rom_file):
@@ -7,9 +51,9 @@ def decompress_asset(rom_file):
     # skip_first_byte
     _ = rom_file.read(1)
     pos = rom_file.tell()
-    print(f'{pos + 0xC00000:#06x}')
-
     while True:
+        current_pos = rom_file.tell() - pos
+
         control_byte = rom_file.read(1)[0]
 
         if control_byte in [0x1a, 0x1b]:
@@ -40,12 +84,57 @@ def decompress_asset(rom_file):
                     decompressed += bytes([decompressed[back_pointer + l]])
         else:
             decompressed += bytes([control_byte])
-
     return decompressed
 
 
 decompress_gfx_xrefs = [0xee7d46, 0xee850b, 0xee8548, 0xee855d, 0xee8572, 0xee8593,
                         0xeed8a3, 0xeed8b0, 0xeed8bd]
+
+lz_compressed_gfxs = [
+    # 0xcc2d80,
+    # 0xe87432,
+    # 0xe883ec,
+    0xe89a4f
+]
+
+
+def lz_decompress_gfx(rom):
+    size = struct.unpack('<H', rom_file.read(2))[0]
+    # print(f'size {size:#0{6}x}')
+    data = rom_file.read(size)
+    decompressed = lz_decompress(data)
+
+    def get_size(count, control_byte):
+        size = 0
+        for i in range(count):
+            if (control_byte >> i) & 1:
+                size += 2
+            else:
+                size += 1
+        return size + 1
+
+    while True:
+        control = rom.read(1)
+
+        if control[0] != 0:
+            next_end = rom.read(2)
+            control_byte = rom.peek(1)
+
+            next_size = get_size(control[0], control_byte[0])
+            data = rom.read(next_size)
+
+            decompressed = lz_decompress(data, decompressed)
+        else:
+            break
+    # print(f'end {rom.tell():#x}')
+    return decompressed
+
+
+def lz_compress_gfx(data):
+    compressed_data = lz_compress(data, search_patterns=False)
+    compressed_length = len(compressed_data)
+    return struct.pack('<H', compressed_length) + compressed_data + b'\x00'
+
 
 if __name__ == '__main__':
     with open('../bl.sfc', 'rb') as rom_file:
